305169c9aba55f2231f13b8ec96858a00a86d817
Yasmina Khawaja <Yakh1000@stud.ek.dk>
2026-04-13 10:48:19 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.querySelector("#hdhdh").addEventListener("click", blabla)
+function blabla{
+    console.log("blabla");
+    document.querySelector("info-text").textContent = "blabla";
+    document.querySelector(".blabla").textContent = "blabla";
+}
+
+document.querySelector("#tihi").addEventListener("click", blabla)
+function blabla{
+    console.log("blabla");
+    document.querySelector("info-text").textContent = "blabla";
+    document.querySelector(".blabla").textContent = "blabla";
+}
